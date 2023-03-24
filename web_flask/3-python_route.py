e7e4f707320da70e263662238cb5aa5f53856412
gavintobin <gavintobin714@gmail.com>
2023-03-24 14:06:37 -0700

    try1

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""task one starts flask web app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hey_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def heybnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisntfun(text):
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def weluvpy(text):
+    return 'Python' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
