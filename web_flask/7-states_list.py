f4b6fc1231496e1a4f66c5d948cf98f88df6c5d6
gavintobin <gavintobin714@gmail.com>
2023-03-27 07:54:49 -0700

    try1

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""task 7"""
+from flask import Flask, request, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states_list/', strict_slashes=False)
+def listthem():
+    """88888"""
+    states = storage.all('State')
+    render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def storclo():
+    """8 agaain"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
