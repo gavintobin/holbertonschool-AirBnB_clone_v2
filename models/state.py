4a15dd9a1becd0c7ea9234c9dc2512a08deb59d2
gavintobin <gavintobin714@gmail.com>
2023-03-27 07:19:50 -0700

    try1

--- models/state.py
+++ models/state.py
@@ -7,8 +7,20 @@ from os import getenv
 
 
 class State(BaseModel, Base):
-    __tablename__ = 'states'
-    """ State class """  
-    name =  Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='delete')
+    if getenv('HBNB_STORAGE_TYPE') == 'db':
+        __tablename__ = 'states'
+        """ State class """
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='delete')
+    else:
+        name = ''
 
+    @property
+    def cities(self):
+        """7"""
+        import models
+        citlist = []
+        for city in models.storage.all('City').values():
+            if city.state_id == self.id:
+                citlist.append(city)
+        return citlist
