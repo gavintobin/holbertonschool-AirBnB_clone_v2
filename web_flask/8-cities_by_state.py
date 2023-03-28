ba6e20b5ace13daaf48ced87513bc23df18d4fe2
gavintobin <gavintobin714@gmail.com>
2023-03-28 08:06:25 -0700

    tty1

---
+++ web_flask/8-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""task 8"""
+from flask import Flask, request, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_list():
+    '''8'''
+    states = storage.all('State')
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    '''8 part 2'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
